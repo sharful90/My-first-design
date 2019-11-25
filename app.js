4c351baf9669a365338eac53611128067ce38c29
sharful90 <bdlifehacks32@gmail.com>
2019-11-25 15:09:32 +0600

    My first design

---
+++ app.js
@@ -0,0 +1,59 @@
+
+document.write("<br />");
+document.write("<br />");
+
+
+
+
+
+	
+
+	
+	
+
+
+var fruits =["banana","apple","mango","orange"] 
+	document.querySelector(".demo1").innerHTML =fruits;
+
+function myFunction(){
+	
+	fruits.push("lemon");
+	document.querySelector(".demo1").innerHTML=fruits;
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+আম্মার
+
+
+
+
+
+
+
+
+
+
+
